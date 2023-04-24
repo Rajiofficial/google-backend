4b4c6fa977be3afe2f6c4ca45ef32e6e8734ba1a
demo <rajfernanto764@gmail.com>
2023-04-24 12:58:32 +0530

    first commit

--- index.js
+++ index.js
@@ -12,55 +12,43 @@ app.use("/",router)
 
 connect()
 
-const student=[
 
-{
-id:"1",
-name:"raji",
-department:"msc" 
-},
-{
-    id:"2",
-name:"ranjith",
-department:"msc" 
-}
-]
 
 app.get('/',(req,res)=>{
       res.status(200).send(student)
   })
   
 
-app.get('/:id',(req,res)=>{
-  try{ 
-  let id=req.params.id
-  console.log(id)
-  let data=[]
- data=student.filter(stu=>stu.id===id)
-    res.status(200).send(data)
-  }catch{
-    res.status(401).send("error")
-  }
-})
+// app.get('/:id',(req,res)=>{
+//   try{ 
+//   let id=req.params.id
+//   console.log(id)
+//   let data=[]
+//  data=student.filter(stu=>stu.id===id)
+//     res.status(200).send(data)
+//   }catch{
+//     res.status(401).send("error")
+//   }
+// })
 
 
-app.post("/find",(req,res)=>{
-   try{
-    console.log("post request",req.body)
-    let studentsname= new guestes(req.body)
-    studentsname.save((err,data)=>{
-        if(err || !data){
-          console.log(err)
-            return {status:(400),message:"error the data"}
+// app.post("/find",(req,res)=>{
+//    try{
+//     console.log("post request",req.body)
+//     let studentsname= new guestes(req.body)
+//     studentsname.save((err,data)=>{
+//         if(err || !data){
+//           console.log(err)
+//             return {status:(400),message:"error the data"}
            
-        }
-        return res.status(201).send(data)
+//         }
+//         return res.status(201).send(data)
     
-    })
-}catch(err){
-    console.log("error100")
-}
-})
+//     })
+// }catch(err){
+//     console.log("error100")
+// }
+// })
 
 let port = process.env.PORT | 3001 
 
