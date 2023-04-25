cae968db5b8476c43c69488f45115880de3a6a24
demo <rajfernanto764@gmail.com>
2023-04-25 12:14:41 +0530

    first commit

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const { router }=require('./router')
 const {connect}=require('./dbconnection')
 require("dotenv").config()
 const cors=require('cors')
-// const students=require("./routes/students")
 
 const app=express();
 app.use(cors())
@@ -11,37 +10,6 @@ app.use(express.json())
 app.use("/",router)
 
 connect()
-  
-// app.get('/:id',(req,res)=>{
-//   try{ 
-//   let id=req.params.id
-//   console.log(id)
-//   let data=[]
-//  data=student.filter(stu=>stu.id===id)
-//     res.status(200).send(data)
-//   }catch{
-//     res.status(401).send("error")
-//   }
-// })
-
-
-// app.post("/find",(req,res)=>{
-//    try{
-//     console.log("post request",req.body)
-//     let studentsname= new guestes(req.body)
-//     studentsname.save((err,data)=>{
-//         if(err || !data){
-//           console.log(err)
-//             return {status:(400),message:"error the data"}
-           
-//         }
-//         return res.status(201).send(data)
-    
-//     })
-// }catch(err){
-//     console.log("error100")
-// }
-// })
 
 let port = process.env.PORT | 3001 
 
--- scehmamodel.js
+++ scehmamodel.js
@@ -3,10 +3,7 @@ const models=new mongoose.Schema({
     mail_id:{
           type:String,
           unique:true,
-          // trim:true,
-          // required:'email address is required',
-          // validate:'please fill up valid email address',
-          
+        
     }
 })
 module.exports=mongoose.model("scehmamodel",models)
